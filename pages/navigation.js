abe29f246ece665b2ae3d06c969452a78d553871
Swetha Saseendran <68152189+swetha4444@users.noreply.github.com>
2022-02-16 03:20:28 +0530

    subnav navigation

--- pages/navigation.js
+++ pages/navigation.js
@@ -3,6 +3,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React, {Component} from 'react';
 import * as ReactBootStrap from "react-bootstrap";
 import Link from "next/link"
+import { Menu, MenuItem, MenuButton, SubMenu } from '@szhsin/react-menu';
 import {
     Box,
     Stack,
@@ -11,6 +12,27 @@ import {
     useDisclosure
   } from "@chakra-ui/react";
 import { HamburgerIcon } from '@chakra-ui/icons'
+
+
+const menuItemStyles = {
+    color: 'blue',
+    fontSize: '1.2rem',
+    fontFamily: 'monospace',
+    color: '#34dd5e',
+    textAlign: 'center',
+    textDecorationStyle:'dotted',
+    border: '#373808',
+    borderStyle:'groove ',
+    borderWidth: '5px',
+    backgroundColor: 'black',
+    hover: {
+      color: '#ee1',
+      backgroundColor: '#bf4080'
+    },
+    active: {
+      backgroundColor: '#333'
+    }
+};
 
 const Navigation = (props) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
@@ -50,7 +72,13 @@ const Navigation = (props) => {
             mt={{ base: 4, md: 0 }}
             >
                 <Link href="."><a className={styles.link}>HOME</a></Link>
-                <Link href="./projects"><a className={styles.link}>WORKS</a></Link>
+                <Link href="./projects"><a className={styles.link}>
+                    <Menu menuButton={<button>WORKS</button>} menuStyles={menuItemStyles}>
+                        <MenuItem><Link href="./projects#projectsID">Projects</Link></MenuItem>
+                        <MenuItem><Link href="./projects#certificatesID">Certificates</Link></MenuItem>
+                        <MenuItem><Link href="./projects#publicationsID">Publications</Link></MenuItem>
+                    </Menu>  
+                </a></Link>
                 <Link href="./experience"><a className={styles.link}>EXPERIENCE</a></Link>
                 <a href="https://cdn.discordapp.com/attachments/758339851996233730/939287991132778506/Swetha_Saseendran_Resume.pdf" className={styles.link} download>RESUME</a>
             </Stack>
